efb8538bf49d5e00c697645b37a98c43073a99d9
Aghakarim-23 <hemidzade1010@gmail.com>
2026-02-05 03:48:09 +0400

    add first route for controlling works || not

--- src/server.js
+++ src/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 8001;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
