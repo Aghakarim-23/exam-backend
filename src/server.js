7616c1e33b10ca2cf5e4ff322dbab2786103e23f
Aghakarim-23 <hemidzade1010@gmail.com>
2026-02-05 03:42:17 +0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
