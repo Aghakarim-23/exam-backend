89ad790f5136707e5be9906422f45bf0f28d2ca8
Aghakarim-23 <hemidzade1010@gmail.com>
2026-03-14 16:36:31 +0400

    add: login controller added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -41,3 +41,44 @@ export const register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, 
+      sameSite: "strict",
+      maxAge: 60 * 60 * 1000 // 1 hour
+    });
+
+    res.json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
